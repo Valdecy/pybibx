85417a0bffd051cbced468d7063e9cbcb50807d3
Valdecy Pereira <valdecypereira@yahoo.com.br>
2026-05-03 16:40:18 -0300

    Add files via upload

--- pybibx/__init__.py
+++ pybibx/__init__.py
@@ -1,1 +1,19 @@
+from .base.pbx import pbx_probe
 
+bibliometrix = pbx_probe
+
+__all__ = [
+    "pbx_probe",
+    "web_app",
+    "web_stop",
+]
+
+
+def web_app(*args, **kwargs):
+    from .base.app import web_app as _web_app
+    return _web_app(*args, **kwargs)
+
+
+def web_stop(*args, **kwargs):
+    from .base.app import web_stop as _web_stop
+    return _web_stop(*args, **kwargs)
